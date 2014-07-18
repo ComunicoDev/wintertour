ef0af2163b5dbeffa2f29b8d0fb89f9372111ee7
Zorgatone <zorgatone@gmail.com>
2014-07-18 10:26:52 +0200

    Fixed stupid error.. Code merged into one file

--- wintertour.php
+++ wintertour.php
@@ -1,5 +1,13 @@
 <?php
 	/**
+	 *
+	 * Gestionale WinterTour
+	 *
+	 * Plugin per gestire i tornei e l'anagrafica e le iscrizioni dei membri
+	 * @author Comunico S.r.l. <info@comunico.info>
+	 * @version 1.0
+	 * @package wintertour
+	 *
 	 * Plugin Name: Gestionale WinterTour
 	 * Plugin URI: http://www.sporthappenings.it/gestionale/
 	 * Description: Plugin per gestire i tornei e l'anagrafica e le iscrizioni dei membri
@@ -8,10 +16,20 @@
 	 * Author URI: http://www.comunico.info/
 	 * License: GNU General Public License v2 or later
 	 * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
-	 * 
+	 *
 	 * This plugin, like WordPress, is licensed under the GPL.
 	 */
 	
+	/**
+	 * Admin menu handler
+	 */
+	function wintertour_admin() {
+		include ('wintertour_import_admin.php');
+	}
+	
+	/**
+	 * Add options page handler
+	 */
 	function wintertour_admin_actions() {
 		add_options_page("Gestionale WinterTour", "Gestionale WinterTour", 1, "Gestionale WinterTour", "wintertour_admin");
 	}
--- wintertour_import_admin.php
+++ wintertour_import_admin.php
@@ -1,11 +1,3 @@
 <?php
-	function wintertour_admin() {
-		include ('wintertour_import_admin.php');
-	}
 	
-	function wintertour_admin_actions() {
-		add_options_page("Gestionale WinterTour", "Gestionale WinterTour", 1, "Gestionale WinterTour", "wintertour_admin");
-	}
-	
-	add_action('admin_menu', 'wintertour_admin_actions');
 ?>
